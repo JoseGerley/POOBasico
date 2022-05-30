1db5a070cdf1df3c21592e8897d222915c0f273f
JoseGerley <1193260391@u.icesi.edu.co>
2022-05-30 15:59:55 -0500

    Fix some errors on Java code

--- Java/Main.java
+++ Java/Main.java
@@ -1,12 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Car car = new Car("ABC-1234",new Account("Jose da Silva", "16847224")); 
-        car.passenger = 4;
-        car.printDataCar();
-
+        UberX uberX = new UberX("ABC-1234", new Account("Jose da Silva", "16847224"), "Chevrolet", "Onix"); 
+        uberX.passenger = 3;
+        uberX.printDataCar();
+        /*
         Car car2 = new Car("DEF-5678",new Account("Maria da Silva", "10495327"));
         car2.passenger = 2;
-        car.printDataCar();
+        uberX.printDataCar();
+        */
     }
 }
--- Java/UberBan.java
+++ Java/UberBan.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class UberBan extends Car {
 
-    Map<String, ArrayList<String,Integer>> typeCarAccepted;
+    Map<String, Map<String,Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
     
     
     public UberBan(String license, Account driver,
-    Map<String, ArrayList<String,Integer>> typeCarAccepted, ArrayList<String> seatsMaterial) {
+    Map<String, Map<String,Integer>> typeCarAccepted, ArrayList<String> seatsMaterial) {
         super(license, driver);
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
