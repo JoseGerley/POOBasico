92406daa9dab0ff10161706e3479e1eec46b4ae6
JoseGerley <1193260391@u.icesi.edu.co>
2022-05-30 15:21:43 -0500

    The concept of inheritance in javascript has been added

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,4 +2,6 @@ var car = new Car("ABC-1234", new Account("Juan", "123.456.789-10"))
 car.passanger = 4
 car.printDataCar()
 
-
+var uberX = new UberX("DEF-5678",new Account("Pedro", "988.456.456-10"), "Chevrolet", "Onix")
+uberX.passanger = 4
+uberX.printDataCar()
